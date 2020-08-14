b51304d0b01a6d5c0b67b0160c51d20eb4580c5c
paulycchou <69647640+paulycchou@users.noreply.github.com>
2020-08-14 11:01:39 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("My name is Paul Chou");
+	    System.out.println("I plan on majoring in chemistry or biochemistry");
+	    System.out.println("My hobbie are playing basekball and solving Rubix's Cube");
+	    System.out.println("I like any type of videos game that have HP system");
+	    System.out.println("I want to learn CS because I believe that it is essential in the future");
+	    System.out.println("I plan on going to a good university");
+    }
+}
